2093207ccb4beaaa21322a43df1f8edab190a028
Wale Adeyeye <wale@propsplanet.com>
2016-04-13 13:16:38 +0100

    wud-frontend-techtest
    - Added dynamic menu names to navbar component
    - Added underscore as dependency to bower
    
    Signed-off-by: Wale Adeyeye <ao.adewale@gmail.com>

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -5,11 +5,45 @@
     .module('wud.techtest')
     .directive('wudNavbar', wudNavbar);
 
-  /** @ngInject */
+  /**
+   * [wudNavbar [wud-navbar] directive function
+   * @return {Object}
+   */
   function wudNavbar() {
     return {
       restrict: 'E',
-      templateUrl: 'app/components/navbar/navbar.html'
+      templateUrl: 'app/components/navbar/navbar.html',
+      
+      controller: function($scope, $state, $location, $log) {
+        $scope.navBarMenuNames = [];
+
+        /**
+         * Uses a map function to filter out and store a new array
+         * of menu names and state names from $stateProvider
+         * get() function which returns an array of all defined states 
+         */
+        var tempNames = $state.get().map(
+          function(obj) {
+            var newObj = {};
+              newObj.sref = obj.name;
+              newObj.name = obj.name.split('.')[1];
+           return newObj;
+        });
+
+        /**
+         * Uses underscope _filter helper function to filter and 
+         * return valid objects. Remove undefined objects and
+         * returns only the defined ones.
+         */
+        $scope.navBarMenuNames = _.filter(tempNames, function(obj) {
+          if(angular.isDefined(obj.name)) {
+            return obj;
+          }
+        });
+
+        //$log.info($scope.navBarMenuNames);
+      }
+
     };
   }
 })();
